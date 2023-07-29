9a7a9d2a9be048113f45a4eb9139e95559f37a8e
David Heimann <d@dmeh.net>
2023-07-29 01:45:16 -0400

    Show spaces / tabs correctly

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,3 +2,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 
+vim.opt.expandtab = true  -- Use spaces instead of tabs.
+vim.opt.tabstop = 2  -- Number of spaces that a <Tab> in the file counts for.
+vim.opt.shiftwidth = 2  -- Number of spaces to use for each step of (auto)indent.
+vim.opt.softtabstop = 2  -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>.
+
+
