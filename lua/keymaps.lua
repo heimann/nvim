baa169145abf7f1828ad33cf04db7308e6a11210
David Heimann <d@dmeh.net>
2023-07-29 01:43:36 -0400

    Format keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,21 +3,21 @@ local wk = require("which-key")
 
 -- Set up your mappings
 wk.register({
-  -- Reload Configuration
-  ["<CR>"] = { "<cmd>lua ReloadConfig()<CR>", "Reload Configuration" },
-  
-  -- Telescope mappings
-  f = {
-    name = "+find",
-    f = { "<cmd>Telescope find_files<CR>", "Find Files" },
-    w = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
-    b = { "<cmd>Telescope buffers<CR>", "Buffers" },
-  },
-  
-  -- Git
-  g = {
-    name = "+git",
-    s = { "<cmd>Git<CR>", "Git Status" },
-    -- add more Git related commands here
-  },
+	-- Reload Configuration
+	["<CR>"] = { "<cmd>lua ReloadConfig()<CR>", "Reload Configuration" },
+
+	-- Telescope mappings
+	f = {
+		name = "+find",
+		f = { "<cmd>Telescope find_files<CR>", "Find Files" },
+		w = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
+		b = { "<cmd>Telescope buffers<CR>", "Buffers" },
+	},
+
+	-- Git
+	g = {
+		name = "+git",
+		s = { "<cmd>Git<CR>", "Git Status" },
+		-- add more Git related commands here
+	},
 }, { prefix = "<leader>" })
