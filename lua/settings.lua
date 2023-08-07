6d8b466df0d99d20d5b39578e3f9aa11a4e2d92a
David Heimann <d@dmeh.net>
2023-08-07 19:10:04 -0400

    Update settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,10 +1,40 @@
-vim.opt.number = true
+-- Display Settings
+vim.opt.number = true               -- Line numbers
 vim.opt.relativenumber = true
-vim.opt.clipboard = "unnamedplus"
+vim.opt.hidden = true               -- Hide buffers without closing
+vim.opt.wrap = false                -- No line wrapping
+vim.opt.encoding = "utf-8"          -- Display encoding
+vim.opt.fileencoding = "utf-8"      -- File encoding
+vim.opt.ruler = true                -- Cursor position display
+vim.opt.cmdheight = 2               -- Height for command messages
+vim.opt.termguicolors = true        -- Enable true colors
 
-vim.opt.expandtab = true  -- Use spaces instead of tabs.
-vim.opt.tabstop = 2  -- Number of spaces that a <Tab> in the file counts for.
-vim.opt.shiftwidth = 2  -- Number of spaces to use for each step of (auto)indent.
+-- Search Settings
+vim.opt.incsearch = true            -- Incremental search
+vim.opt.smartcase = true            -- Case-insensitive search until uppercase character is supplied
+
+-- Tab and Indentation
+vim.opt.tabstop = 2                 -- Tab width
+vim.opt.shiftwidth = 2              -- Indent width
 vim.opt.softtabstop = 2  -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>.
+vim.opt.smarttab = true             -- Intelligent tabbing
+vim.opt.expandtab = true            -- Convert tabs to spaces
+vim.opt.smartindent = true          -- Smart auto-indentation
+vim.opt.autoindent = true           -- Auto indentation
 
+-- Split Settings
+vim.opt.splitbelow = true           -- Horizontal split direction
+vim.opt.splitright = true           -- Vertical split direction
+
+-- Misc Settings
+vim.opt.iskeyword:append("-")       -- Word delimiters
+vim.opt.showtabline = 2             -- Always show tab bar
+vim.opt.showmode = false            -- No mode display
+vim.opt.updatetime = 300            -- Cursor hold update time
+vim.opt.timeoutlen = 500            -- Key mapping timeout
+vim.opt.formatoptions:remove("cro") -- Comment formatting
+vim.opt.backup = false              -- No backup files
+vim.cmd[[ set spell! ]]             -- Toggle spellcheck
+vim.opt.spelllang = {"en", "de"}    -- Spellcheck languages
+vim.opt.clipboard = "unnamedplus"
 
