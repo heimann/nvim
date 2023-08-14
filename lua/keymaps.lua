97cb426dac436a68df5c10af3d1a0c7b2c7b750f
David Heimann <d@dmeh.net>
2023-08-14 14:30:24 -0400

    Use tab to quickly switch between buffers

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,6 +3,8 @@ map("n", "<C-h>", ":TmuxNavigateLeft<CR>", { noremap = true, silent = false })
 map("n", "<C-j>", ":TmuxNavigateDown<CR>", { noremap = true, silent = false })
 map("n", "<C-k>", ":TmuxNavigateUp<CR>", { noremap = true, silent = false })
 map("n", "<C-l>", ":TmuxNavigateRight<CR>", { noremap = true, silent = false })
+map("n", "<Tab>", ":b#<CR>", { noremap = true, silent = false })
+
 
 -- Remove higlighting
 map("n", "<leader><space>", ":nohl<CR>", { noremap = true })
