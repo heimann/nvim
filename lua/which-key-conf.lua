1bcd26cd3e0395393fe3fac03a97c34b9bfa9df7
David Heimann <d@dmeh.net>
2023-10-31 23:14:27 -0400

    Add more keybinds for chat gpt stuff

--- lua/which-key-conf.lua
+++ lua/which-key-conf.lua
@@ -17,6 +17,24 @@ wk.register({
     b = { "<cmd>Telescope buffers<CR>", "Buffers" },
   },
 
+  -- Chat GPT
+  c = {
+  name = "ChatGPT",
+    c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+    e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+    g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+    t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+    k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+    d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+    a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+    o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+    s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+    f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+    x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+    r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+    l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+  },
+
   -- Git
   g = {
     name = "+git",
@@ -24,21 +42,5 @@ wk.register({
     h = { "<cmd>GBrowse!<CR>", "Copy Github link" },
   },
 }, { prefix = "<leader>" })
-
-wk.register({
-  c = {
-    name = "ChatGPT",
-    e = {
-      function()
-        chatgpt.edit_with_instructions()
-      end,
-      "Edit with instructions",
-    },
-  },
-}, {
-  prefix = "<leader>",
-  mode = "v",
-})
-
 
 vim.api.nvim_set_keymap("v", "<leader>gh", ":GBrowse!<cr>", { noremap = true })
