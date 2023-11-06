e9d7d54eed0f246d6354a0218df3839f5915dbfc
David Heimann <d@dmeh.net>
2023-11-05 21:17:32 -0500

    Change telescope file finder to default to dropdown

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -262,6 +262,15 @@ local plugins = {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.2',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          }
+        }
+      })
+    end
   },
   {
     "folke/flash.nvim",
