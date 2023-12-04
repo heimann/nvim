bbeb72932a83ae69835bf706e40192a41f478b09
David Heimann <d@dmeh.net>
2023-12-03 22:32:46 -0500

    Move more plugins to new conf

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,4 +3,7 @@ return {
     "williamboman/mason.nvim",
     "neovim/nvim-lspconfig",
     "williamboman/mason-lspconfig.nvim",
+    "nvim-treesitter/nvim-treesitter-context",
+    "tpope/vim-commentary",
+    "kshenoy/vim-signature",
 }
