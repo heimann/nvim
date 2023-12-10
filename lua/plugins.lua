3ba7aaf125b30483bcda5184066fe49069a9cfbb
David Heimann <d@dmeh.net>
2023-12-10 13:25:17 -0500

    Add conform for formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -38,6 +38,45 @@ local plugins = {
     lazy = false,
   },
   {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        elixir = { "mix format" },
+        sql = {"sql-formatter"},
+        javascript = { { "prettierd", "prettier" } },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
   },
